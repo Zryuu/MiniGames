8a3caf15c29110ffd17159f2f06811c9db772e18
Zryuu <Zryu323@gmail.com>
2024-07-15 15:16:28 -0500

    Adding MMCard.cs

--- SamplePlugin/Games/MemoryMatch/MMCard.cs
+++ SamplePlugin/Games/MemoryMatch/MMCard.cs
@@ -3,7 +3,7 @@
 namespace SamplePlugin.Games.MemoryMatch;
 
 
-enum ECardFace
+enum ECardType
 {
     Blue,
     Red,
@@ -11,16 +11,36 @@ enum ECardFace
     Yellow
 }
 
+enum ECardFace
+{
+    FaceUp,
+    FaceDown
+}
+
 public class MMCard
 {
+    private ECardType cardType;
     private ECardFace cardFace;
     private readonly ISharedImmediateTexture BlueFace;
     private readonly ISharedImmediateTexture RedFace;
     private readonly ISharedImmediateTexture GreenFace;
     private readonly ISharedImmediateTexture YellowFace;
-    
-    
-    
-    
+
+    public MMCard()
+    {
+        
+        
+        
+    }
+
+
+    private void SwapFace()
+    {
+        cardFace = cardFace switch
+        {
+            ECardFace.FaceDown => ECardFace.FaceUp,
+            ECardFace.FaceUp => ECardFace.FaceDown,
+        };
+    }
 
 }
